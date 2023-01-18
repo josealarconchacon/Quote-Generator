190242ba43394723b50464199cc20dc01600c490
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-01-18 13:29:23 -0500

    move script.js into js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let apiQuotes = [];
+
+// display new quote
+function newQuote() {
+  // pick a random quote from apiQuotes array
+  const quotes = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quotes);
+}
+
+// fetch quotes from api
+async function fetchQuotes() {
+  const url = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(url);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // error handler
+  }
+}
+
+// on load
+fetchQuotes();
